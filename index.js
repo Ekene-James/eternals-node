e4c9d2f8f42e1c0977215ed095009c746b0ce27e
james ekene <edehjamesraphael@gmail.com>
2022-04-18 20:15:04 +0100

    cors5

--- index.js
+++ index.js
@@ -19,9 +19,8 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 
 //body parser
-app.use(express.json());
+//app.use(express.json());
 
-app.use(allowCors());
 //app.use(cors());
 
 
@@ -31,16 +30,17 @@ if (process.env.NODE_ENV === "development") {
 //connect to db
 const db = require("./config/keys/keys").mongoURI;
 mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  })
-  .then(connections => {
-    console.log(`mongodb connected `.cyan.underline.bold);
-  })
-  .catch(err => console.log(`${err.message}`.red.bold));
+.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+.then(connections => {
+  console.log(`mongodb connected `.cyan.underline.bold);
+})
+.catch(err => console.log(`${err.message}`.red.bold));
 
+app.use(allowCors());
   // app.use(function(req, res, next) {
   //   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
