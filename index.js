7f4a760715fc12e2f0bd5881a952a6384f4074df
james ekene <edehjamesraphael@gmail.com>
2022-04-18 14:13:02 +0100

    cors slight change

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ mongoose
   })
   .catch(err => console.log(`${err.message}`.red.bold));
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  // app.use(function(req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // });
 
 //mount the routers
 app.use("/api/storePayments", storePayments);
