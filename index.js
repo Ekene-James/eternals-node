7f00afce40503711fbed752bd3f05f425e07496b
james ekene <edehjamesraphael@gmail.com>
2022-04-18 15:35:55 +0100

    cors 2

--- index.js
+++ index.js
@@ -70,9 +70,16 @@ process.on("unhandledRejection" || "uncaughtException", (err, promise) => {
   server.close(() => process.exit(1));
 });
 
-// "routes":[
-//     {
-//         "src":"/(.*)",
-//         "dest":"/"
-//     }
-// ],
+
+
+// "headers": [
+//   {
+//     "source": "/(.*)",
+//     "headers": [
+//       { "key": "Access-Control-Allow-Credentials", "value": "true" },
+//       { "key": "Access-Control-Allow-Origin", "value": "*" },
+//       { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+//       { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+//     ]
+//   }
+// ]
