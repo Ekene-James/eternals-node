7f1677b223f09823632b7f0e6cd39dedb1e77c78
james ekene <edehjamesraphael@gmail.com>
2022-04-18 20:18:25 +0100

    cors6

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 //body parser
 //app.use(express.json());
 
-//app.use(cors());
+
 
 
 if (process.env.NODE_ENV === "development") {
@@ -39,8 +39,8 @@ mongoose
   console.log(`mongodb connected `.cyan.underline.bold);
 })
 .catch(err => console.log(`${err.message}`.red.bold));
-
-app.use(allowCors());
+//app.use(cors());
+app.use(allowCors);
   // app.use(function(req, res, next) {
   //   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
